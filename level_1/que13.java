8e3e9e9d1a324e74b4390681417ddecc9df4b0b5
RUDRAKSHA SHARMA <rudraksha.sharma_cs.aiml24@gla.ac.in>
2025-08-29 11:24:29 +0530

    Delete level_1 directory

--- level_1/que13.java
+++
@@ -1,8 +0,0 @@
-/*Write a program that takes the base and height to find area of a triangle in square inches and square centimeters 
-Hint => Area of a Triangle is ½ * base * height
-I/P => base, height
-O/P => Your Height in cm is ___ while in feet is ___ and inches is ___ */
-public class que13 {
-    int base = ystem.in.read();
-    int height = System.in.read();
-}
