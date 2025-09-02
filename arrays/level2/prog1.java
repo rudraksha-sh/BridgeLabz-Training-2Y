bb90a7213691d1daf8dea2b263eccaa2d80eaa3a
RUDRAKSHA SHARMA <rudraksha.sharma_cs.aiml24@gla.ac.in>
2025-09-02 12:25:54 +0530

    [rudraksha] added control flow and arrays

---
+++ arrays/level2/prog1.java
@@ -0,0 +1,11 @@
+package level2;
+/*Write a program to find the age of Harry if the birth year is 2000. Assume the Current Year is 2024
+I/P => NONE
+O/P => Harry's age in 2024 is ___  */
+public class prog1{
+    public static void main(String[] arg){
+        int age = 2024-2000;
+        System.out.println("Harry's age in 2024 is " + age);
+
+    }
+}
